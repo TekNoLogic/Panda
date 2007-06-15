069d81b75da87ec0df9b420c7905e9d51128e2d5
Tekkub Stoutwrithe <tekkub@gmail.com>
2007-06-15 05:22:11 +0000

    Panda - Lots of jizzy goodness in this one.... - Added DE probabilities and quantities for each item - Items show bind type - Added AH buyout prices from wowhead, results show the per-item BO of their line, item shows a total weighted average value
    git-svn-id: http://tekkub-wow.googlecode.com/svn/trunk/Panda@439 86fe6d9a-1522-0410-a387-bf9db416f0a0

--- Disenchanting.lua
+++ Disenchanting.lua
@@ -4,7 +4,9 @@ local tip = DEATinyGratuity
 DEATinyGratuity = nil
 
 
-local NUM_LINES = 11
+local ICONSIZE, NUM_LINES = 36, 7
+local OFFSET = math.floor((305 - NUM_LINES*ICONSIZE)/(NUM_LINES+1))
+
 local showBOP, frame = false
 
 
@@ -22,19 +24,49 @@ local function DEable(link)
 end
 
 
-function Panda:DisenchantBagUpdate(self)
+local function GS(cash)
+	local s = floor(cash%100)
+	local g = floor(cash/100)
+	if g > 0 then return string.format("|cffffd700%d.|cffc7c7cf%02d", g, s)
+	else return string.format("|cffc7c7cf%d", s) end
+end
+
+
+local function GSC(cash)
+	local g, s, c = floor(cash/10000), floor((cash/100)%100), cash%100
+	if g > 0 then return string.format("|cffffd700%d.|cffc7c7cf%02d.|cffeda55f%02d", g, s, c)
+	elseif s > 0 then return string.format("|cffc7c7cf%d.|cffeda55f%02d", s, c)
+	else return string.format("|cffc7c7cf%d", c) end
+end
+
+
+function Panda:DisenchantBagUpdate()
 	local i = 1
+	frame.NoItems:Hide()
 
 	for bag=0,4 do
 		for slot=1,GetContainerNumSlots(bag) do
 			local link = GetContainerItemLink(bag, slot)
-			if link and DEable(link) and (showBOP or not IsBound(bag, slot)) then
+			local bound = IsBound(bag, slot)
+			if link and DEable(link) and (showBOP or not bound) then
 				local name, _, _, itemLevel, _, itemType, itemSubType, _, _, texture = GetItemInfo(link)
+				local id1, qty1, prob1, weight1, id2, qty2, prob2, weight2, id3, qty3, prob3, weight3 = self:GetPossibleDisenchants(link)
+				local bo1, bo2, bo3 = self:GetAHBuyout(id1), self:GetAHBuyout(id2), self:GetAHBuyout(id3)
+				local _, link1 = GetItemInfo(id1)
+				local _, link2 = GetItemInfo(id2)
+				local _, link3 = GetItemInfo(id3)
+				local val = (id1 and weight1*bo1 or 0) + (id2 and weight2*bo2 or 0) + (id3 and weight3*bo3 or 0)
+
 				local l = frame.lines[i]
 				if self.canDisenchant then l:SetAttribute("macrotext", string.format("/cast Disenchant\n/use %s %s", bag, slot)) end
 				l.icon:SetTexture(texture)
 				l.name:SetText(link)
 				l.type:SetText(itemType)
+				l.bind:SetText(bound and "Soulbound" or "Bind on Equip")
+				l.value:SetText(GS(val))
+				l.item1:SetText(link1); l.prob1:SetText(prob1); l.total1:SetText(GS(bo1)); l.qty1:SetText(qty1)
+				l.item2:SetText(link2); l.prob2:SetText(prob2); l.total2:SetText(GS(bo2)); l.qty2:SetText(qty2)
+				l.item3:SetText(link3); l.prob3:SetText(prob3); l.total3:SetText(GS(bo3)); l.qty3:SetText(qty3)
 				l:Show()
 
 				i = i + 1
@@ -43,13 +75,12 @@ function Panda:DisenchantBagUpdate(self)
 		end
 	end
 
-	if i == 1 then frame.NoItems:Show() else frame.NoItems:Hide() end
+	if i == 1 then frame.NoItems:Show() end
 
 	for j=i,NUM_LINES do frame.lines[j]:Hide() end
 end
 
 
-local ICONSIZE = 24
 function Panda:CreateDisenchantingPanel()
 	frame = CreateFrame("Frame", nil, OptionHouseOptionsFrame)
 	frame:SetWidth(630)
@@ -67,15 +98,15 @@ function Panda:CreateDisenchantingPanel()
 	frame.BOP:SetPoint("TOPLEFT", frame, "BOTTOMLEFT", 0, -4)
 	frame.BOPlabel = frame.BOP:CreateFontString(nil, "ARTWORK", "GameFontNormalSmall")
 	frame.BOPlabel:SetPoint("LEFT", frame.BOP, "RIGHT", 5, 0)
-	frame.BOPlabel:SetText("Show BoP Items")
+	frame.BOPlabel:SetText("Show soulbound items")
 	frame.BOP:SetScript("OnClick", function() showBOP = not showBOP; self:DisenchantBagUpdate(self) end)
 
 	frame.lines = {}
 	for i=1,NUM_LINES do
 		local f = CreateFrame("CheckButton", "DEADEFrame"..i, frame, "SecureActionButtonTemplate")
-		f:SetPoint("TOPLEFT", frame, 2, ICONSIZE-i*(ICONSIZE+3)+1)
+		f:SetPoint("TOPLEFT", frame, OFFSET, ICONSIZE-i*(ICONSIZE+OFFSET))
 		f:SetHeight(ICONSIZE)
-		f:SetWidth(300)
+		f:SetWidth(250)
 		if self.canDisenchant then f:SetAttribute("type", "macro") end
 
 		f.icon = f:CreateTexture(nil, "ARTWORK")
@@ -87,7 +118,59 @@ function Panda:CreateDisenchantingPanel()
 		f.name:SetPoint("TOPLEFT", f.icon, "TOPRIGHT", 5, 0)
 
 		f.type = f:CreateFontString(nil, "ARTWORK", "GameFontHighlightSmall")
-		f.type:SetPoint("BOTTOMLEFT", f.icon, "BOTTOMRIGHT", 5, 0)
+		f.type:SetPoint("LEFT", f.icon, "RIGHT", 5, 0)
+
+		f.bind = f:CreateFontString(nil, "ARTWORK", "GameFontHighlightSmall")
+		f.bind:SetPoint("RIGHT", f, "RIGHT", -5, 0)
+		f.bind:SetText("Bind on Equip")
+
+		f.valuelabel = f:CreateFontString(nil, "ARTWORK", "GameFontHighlightSmall")
+		f.valuelabel:SetPoint("BOTTOMLEFT", f.icon, "BOTTOMRIGHT", 5, 0)
+		f.valuelabel:SetText("Estimated DE value:")
+
+		f.value = f:CreateFontString(nil, "ARTWORK", "GameFontHighlightSmall")
+		f.value:SetPoint("BOTTOMRIGHT", f, "BOTTOMRIGHT", -5, 0)
+		f.value:SetText("999.99")
+
+		local dx = 630-OFFSET*2
+		f.total1 = f:CreateFontString(nil, "ARTWORK", "GameFontHighlightSmall")
+		f.total1:SetPoint("TOPRIGHT", f, "TOPLEFT", dx, 0)
+--~ 		f.total1:SetJustifyH("RIGHT")
+
+		f.total2 = f:CreateFontString(nil, "ARTWORK", "GameFontHighlightSmall")
+		f.total2:SetPoint("RIGHT", f, "LEFT", dx, 0)
+--~ 		f.total2:SetJustifyH("RIGHT")
+
+		f.total3 = f:CreateFontString(nil, "ARTWORK", "GameFontHighlightSmall")
+		f.total3:SetPoint("BOTTOMRIGHT", f, "BOTTOMLEFT", dx, 0)
+--~ 		f.total3:SetJustifyH("RIGHT")
+
+		f.item1 = f:CreateFontString(nil, "ARTWORK", "GameFontHighlightSmall")
+		f.item1:SetPoint("TOPRIGHT", f, "TOPLEFT", dx-150, 0)
+
+		f.item2 = f:CreateFontString(nil, "ARTWORK", "GameFontHighlightSmall")
+		f.item2:SetPoint("RIGHT", f, "LEFT", dx-150, 0)
+
+		f.item3 = f:CreateFontString(nil, "ARTWORK", "GameFontHighlightSmall")
+		f.item3:SetPoint("BOTTOMRIGHT", f, "BOTTOMLEFT", dx-150, 0)
+
+		f.prob1 = f:CreateFontString(nil, "ARTWORK", "GameFontHighlightSmall")
+		f.prob1:SetPoint("TOPRIGHT", f, "TOPLEFT", dx-80, 0)
+
+		f.prob2 = f:CreateFontString(nil, "ARTWORK", "GameFontHighlightSmall")
+		f.prob2:SetPoint("RIGHT", f, "LEFT", dx-80, 0)
+
+		f.prob3 = f:CreateFontString(nil, "ARTWORK", "GameFontHighlightSmall")
+		f.prob3:SetPoint("BOTTOMRIGHT", f, "BOTTOMLEFT", dx-80, 0)
+
+		f.qty1 = f:CreateFontString(nil, "ARTWORK", "GameFontHighlightSmall")
+		f.qty1:SetPoint("TOPRIGHT", f, "TOPLEFT", dx-40, 0)
+
+		f.qty2 = f:CreateFontString(nil, "ARTWORK", "GameFontHighlightSmall")
+		f.qty2:SetPoint("RIGHT", f, "LEFT", dx-40, 0)
+
+		f.qty3 = f:CreateFontString(nil, "ARTWORK", "GameFontHighlightSmall")
+		f.qty3:SetPoint("BOTTOMRIGHT", f, "BOTTOMLEFT", dx-40, 0)
 
 		frame.lines[i] = f
 	end
@@ -103,3 +186,6 @@ function Panda:CreateDisenchantingPanel()
 
 	return frame
 end
+
+
+
---
+++ DisenchantingProb.lua
@@ -0,0 +1,93 @@
+﻿
+
+-- I am lazy, so I "borrowed" these constants from Enchantrix ^^
+local VOID, NEXUS = 22450, 20725
+local LRADIANT, SBRILLIANT, LBRILLIANT, SPRISMATIC, LPRISMATIC = 11178, 14343, 14344, 22448, 22449
+local SGLIMMERING, LGLIMMERING, SGLOWING, LGLOWING, SRADIANT = 10978, 11084, 11138, 11139, 11177
+local LNETHER, GNETHER, LETERNAL, GETERNAL, LPLANAR, GPLANAR = 11174, 11175, 16202, 16203, 22447, 22446
+local LMAGIC, GMAGIC, LASTRAL, GASTRAL, LMYSTIC, GMYSTIC = 10938, 10939, 10998, 11082, 11134, 11135
+local STRANGE, SOUL, VISION, DREAM, ILLUSION, ARCANE = 10940, 11083, 11137, 11176, 16204, 22445
+
+
+-- Average AH Buyouts, mined from wowhead.com
+local buyouts = {
+	[VOID] = 5000, [NEXUS] = 720,
+	[LBRILLIANT] = 1000, [LGLIMMERING] = 100, [LGLOWING] = 150, [LPRISMATIC] = 1500, [LRADIANT] = 780,
+	[SBRILLIANT] = 150, [SGLIMMERING] = 25, [SGLOWING] = 50, [SPRISMATIC] = 500, [SRADIANT] = 600,
+	[GASTRAL] = 100, [GETERNAL] = 1000, [GMAGIC] = 50, [GMYSTIC] = 100, [GNETHER] = 500, [GPLANAR] = 500,
+	[LASTRAL] = 33, [LETERNAL] = 350, [LMAGIC] = 20, [LMYSTIC] = 50, [LNETHER] = 200, [LPLANAR] = 200,
+	[ARCANE] = 200, [DREAM] = 75, [ILLUSION] = 200, [SOUL] = 20, [STRANGE] = 10, [VISION] = 50,
+}
+
+
+-- Only 4 items are disenchantable, rare, ilvl 66-70, and from TBC
+local BCblues = {[23835] = true, [23836] = true, [25653] = true, [32863] = true}
+
+
+local function GetUncommonVals(ilvl)
+	if ilvl <= 15 then return      STRANGE, "1-2x", "80%", 1.5, .80,   LMAGIC, "1-2x", "20%", 1.5, .20
+	elseif ilvl <= 20 then return  STRANGE, "2-3x", "75%", 2.5, .75,   GMAGIC, "1-2x", "20%", 1.5, .20, SGLIMMERING, "1x", "5%", .05
+	elseif ilvl <= 25 then return  STRANGE, "4-6x", "75%", 5.0, .75,  LASTRAL, "1-2x", "15%", 1.5, .15, SGLIMMERING, "1x", "10%", .1
+	elseif ilvl <= 30 then return     SOUL, "1-2x", "75%", 1.5, .75,  GASTRAL, "1-2x", "20%", 1.5, .20, LGLIMMERING, "1x", "5%", .05
+	elseif ilvl <= 35 then return     SOUL, "2-5x", "75%", 3.5, .75,  LMYSTIC, "1-2x", "20%", 1.5, .20,    SGLOWING, "1x", "5%", .05
+	elseif ilvl <= 40 then return   VISION, "1-2x", "75%", 1.5, .75,  GMYSTIC, "1-2x", "20%", 1.5, .20,    LGLOWING, "1x", "5%", .05
+	elseif ilvl <= 45 then return   VISION, "2-5x", "75%", 3.5, .75,  LNETHER, "1-2x", "20%", 1.5, .20,    SRADIANT, "1x", "5%", .05
+	elseif ilvl <= 50 then return    DREAM, "1-2x", "75%", 1.5, .75,  GNETHER, "1-2x", "20%", 1.5, .20,    LRADIANT, "1x", "5%", .05
+	elseif ilvl <= 55 then return    DREAM, "2-5x", "75%", 3.5, .75, LETERNAL, "1-2x", "20%", 1.5, .20,  SBRILLIANT, "1x", "5%", .05
+	elseif ilvl <= 60 then return ILLUSION, "1-2x", "75%", 1.5, .75, GETERNAL, "1-2x", "20%", 1.5, .20,  LBRILLIANT, "1x", "5%", .05
+	elseif ilvl <= 65 then return ILLUSION, "2-5x", "75%", 3.5, .75, GETERNAL, "2-3x", "20%", 2.5, .20,  LBRILLIANT, "1x", "5%", .05
+	elseif ilvl <= 80 then return   ARCANE, "2-3x", "75%", 2.5, .75,  LPLANAR, "1-2x", "20%", 1.5, .20,  SPRISMATIC, "1x", "5%", .05
+	elseif ilvl <= 99 then return   ARCANE, "2-3x", "75%", 2.5, .75,  LPLANAR, "2-3x", "20%", 2.5, .20,  SPRISMATIC, "1x", "5%", .05
+	else return                     ARCANE, "2-5x", "75%", 3.5, .75,  GPLANAR, "1-2x", "20%", 1.5, .20,  LPRISMATIC, "1x", "5%", .05 end
+end
+
+
+function Panda:GetPossibleDisenchants(item)
+	local _, link, qual, ilvl, _, itemtype = GetItemInfo(item)
+	if not link or qual < 2 or (itemtype ~= "Weapon" and itemtype ~= "Armor") then return end
+
+	if qual == 4 then -- Epic
+		if ilvl > 75 and ilvl <= 80 and itemtype == "Weapon" then return NEXUS, "1-2x", "33%/66%", 5/3
+		elseif ilvl <= 45  then return SRADIANT, "2-4x", "100%", 3
+		elseif ilvl <= 50  then return LRADIANT, "2-4x", "100%", 3
+		elseif ilvl <= 55  then return SBRILLIANT, "2-4x", "100%", 3
+		elseif ilvl <= 60  then return NEXUS, "1x", "100%", 1
+		elseif ilvl <= 80  then return NEXUS, "1-2x", "100%", 1.5
+		elseif ilvl <= 100 then return VOID, "1-2x", "100%", 1.5
+		else return VOID, "1-2x", "33%/66%", 5/3 end
+
+	elseif qual == 3 then -- Rare
+		local _, _, itemid = string.find(link, "item:(%d+):")
+		itemid = tonumber(itemid)
+
+		if BCblues[itemid] then return SPRISMATIC, "1x", "99.5%", .995, NEXUS, "1x", "0.5%", 0.005
+		elseif ilvl <= 25 then return SGLIMMERING, "1x", "100%", 1
+		elseif ilvl <= 30 then return LGLIMMERING, "1x", "100%", 1
+		elseif ilvl <= 35 then return    SGLOWING, "1x", "100%", 1
+		elseif ilvl <= 40 then return    LGLOWING, "1x", "100%", 1
+		elseif ilvl <= 45 then return    SRADIANT, "1x", "100%", 1
+		elseif ilvl <= 50 then return    LRADIANT, "1x", "100%", 1
+		elseif ilvl <= 55 then return  SBRILLIANT, "1x", "100%", 1
+		elseif ilvl <= 65 then return  LBRILLIANT, "1x", "100%", 1
+		elseif ilvl <= 70 then return  LBRILLIANT, "1x", "99.5%", .995, NEXUS, "1x", "0.5%", 0.005
+		elseif ilvl <= 99 then return  SPRISMATIC, "1x", "99.5%", .995, NEXUS, "1x", "0.5%", 0.005
+		else return                    LPRISMATIC, "1x", "99.5%", .995, VOID, "1x", "0.5%", 0.005 end
+
+	elseif qual == 2 then -- Uncommon
+		if itemtype == "Armor" then
+			local r1i, r1ta, r1tp, r1a, r1p, r2i, r2ta, r2tp, r2a, r2p, r3i, r3ta, r3tp, r3p = GetUncommonVals(ilvl)
+			return r1i, r1ta, r1tp, r1a*r1p, r2i, r2ta, r2tp, r2a*r2p, r3i, r3ta, r3tp, r3p
+		elseif itemtype == "Weapon" then
+			local r1i, r1ta, r1tp, r1a, r1p, r2i, r2ta, r2tp, r2a, r2p, r3i, r3ta, r3tp, r3p = GetUncommonVals(ilvl)
+			return r1i, r1ta, r2tp, r1a*r2p, r2i, r2ta, r1tp, r2a*r1p, r3i, r3ta, r3tp, r3p
+		end
+	end
+end
+
+
+function Panda:GetAHBuyout(item)
+--~ 	local _, _, itemid = string.find(item, "item:(%d+):")
+--~ 	itemid = tonumber(itemid)
+	return buyouts[item]
+end
+
