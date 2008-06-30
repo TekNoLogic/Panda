86f01b612422b3e138f2e4a996b54d0211ca0d6d
Tekkub <tekkub@gmail.com>
2008-06-29 20:58:16 -0600

    Fix ore tip formatting

--- OreTooltip.lua
+++ OreTooltip.lua
@@ -27,7 +27,7 @@ local OnTooltipSetItem = function(frame, ...)
 	if     id == 23424 then val = (1500 + gemavg(BC_GREEN_GEMS) * 1.027 + gemavg(BC_BLUE_GEMS) * 0.060) * 4
 	elseif id == 23425 then val = (2250 + gemavg(BC_GREEN_GEMS) * 1.100 + gemavg(BC_BLUE_GEMS) * 0.195) * 4 end
 
-	if val and val ~= 0 then frame:AddDoubleLine("Average crush value:", val) end
+	if val and val ~= 0 then frame:AddDoubleLine("Average crush value:", GS(val)) end
 	if origs[frame] then return origs[frame](frame, ...) end
 end
 
