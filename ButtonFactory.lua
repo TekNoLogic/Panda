7e37372791e2252a4329101721f6912a8dcca72b
Tekkub <tekkub@gmail.com>
2008-09-07 03:26:45 -0600

    Make button factory produce working button macros

--- ButtonFactory.lua
+++ ButtonFactory.lua
@@ -49,11 +49,11 @@ function Sadpanda.ButtonFactory(parent, id, secure, notext, ...)
 	count:SetPoint("BOTTOMRIGHT", icon, "BOTTOMRIGHT", -2, 2)
 	f.count = count
 
---~ 		if secure and self.canJC then
+	if secure then
 --~ 			f:SetAlpha(knowncombines[f.name] and 1 or 0.25)
---~ 			f:SetAttribute("type", "macro")
---~ 			f:SetAttribute("macrotext", "/run CloseTradeSkill()\n/cast Jewelcrafting\n/run for i=1,GetNumTradeSkills() do if GetTradeSkillInfo(i) == '"..name.."' then DoTradeSkill(i) end end\n/run CloseTradeSkill()")
---~ 		end
+		f:SetAttribute("type", "macro")
+		f:SetAttribute("macrotext", "/run CloseTradeSkill()\n/cast "..secure.."\n/run for i=1,GetNumTradeSkills() do if GetTradeSkillInfo(i) == '"..name.."' then DoTradeSkill(i) end end\n/run CloseTradeSkill()")
+	end
 
 	return f
 end
