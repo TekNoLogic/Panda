c530d8eef9b4fccd46e5f4f261c36d315add7613
Tekkub <tekkub@gmail.com>
2008-10-14 22:57:36 -0600

    Make ink buttons on milling panel craft the inks

--- Milling.lua
+++ Milling.lua
@@ -1,5 +1,7 @@
 ﻿
 local NAME = GetSpellInfo(51005)
+local NAME2 = GetSpellInfo(45357)
+local inks = "39469 39774 43115 43116 43118 43118 43120 43121 43122 43123 43124 43125 43126 43127"
 Panda.PanelFactory(NAME, 45357,
 [[39151   0   39469   0     0    2447   765  2449   785
   39334 43103 39774 43115   0    2450  2452  3820  2453
@@ -11,6 +13,10 @@ Panda.PanelFactory(NAME, 45357,
   39343 43109 43126 43127   0   36901 36904
 ]], nil, function(frame)
 	frame:SetAttribute("type", "macro")
-	frame:SetAttribute("macrotext", "/cast "..NAME.."\n/use "..frame.name)
+	if inks:match(frame.id) then
+		frame:SetAttribute("macrotext", "/run CloseTradeSkill()\n/cast "..NAME2.."\n/run for i=1,GetNumTradeSkills() do if GetTradeSkillInfo(i) == '"..frame.name.."' then DoTradeSkill(i) end end\n/run CloseTradeSkill()")
+	else
+		frame:SetAttribute("macrotext", "/cast "..NAME.."\n/use "..frame.name)
+	end
 end)
 
