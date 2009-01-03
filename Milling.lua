f7c717cb7b0f28ee0b1568fe4fdb56acdd1a74b0
Tekkub <tekkub@gmail.com>
2009-01-03 01:03:24 -0700

    Hide mass-mill button from players that can't mill

--- Milling.lua
+++ Milling.lua
@@ -11,7 +11,7 @@ Panda.PanelFactory(NAME, 45357,
   39341 43107 43122 43123   0   13464 13463 13465 13466 13467
   39342 43108 43124 43125   0   22785 22786 22787 22789 22790 22791 22792 22793
   39343 43109 43126 43127   0   36901 36903 36904 36905 36906 36907 37921
-]], nil, function(frame)
+]], function(id, frame) if id == 6948 and not GetSpellInfo((GetSpellInfo(45357))) then frame:Hide() end end, function(frame)
 	frame:SetAttribute("type", "macro")
 	if frame.id == 6948 then
 		frame.icon:SetTexture(MILLICON)
